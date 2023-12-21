94dae682cc7d2c8d231017f462be253defdbbb67
Harald G Svendsen <harald.svendsen@sintef.no>
2023-12-21 16:03:10 +0100

    improve map plot with projection

--- src/powergim/plots_map2.py
+++ src/powergim/plots_map2.py
@@ -1,5 +1,7 @@
 import geopandas
 import matplotlib.pyplot as plt
+import pandas as pd
+import pyproj
 import shapely
 
 from powergim.grid_data import GridData
@@ -15,6 +17,8 @@ def plot_map2(
     node_options=None,
     include_generators=None,
     gen_options=None,
+    latlon=None,
+    proj="EPSG:4326",
     **kwargs,
 ):
     """Plot grid using geopandas (for print)"""
@@ -44,11 +48,11 @@ def plot_map2(
         plt.figure(figsize=(10, 10))
         ax = plt.gca()
     ax.set_facecolor("white")
-    lakes.plot(ax=ax, color="white")
-    land.plot(ax=ax, color="lightgray")
-    coastline.plot(ax=ax, color="darkgray")
-    borders.plot(ax=ax, color="darkgray")
-    borders_sea.plot(ax=ax, color="darkgray")
+    lakes.to_crs(proj).plot(ax=ax, color="white")
+    land.to_crs(proj).plot(ax=ax, color="lightgray")
+    coastline.to_crs(proj).plot(ax=ax, color="darkgray")
+    borders.to_crs(proj).plot(ax=ax, color="darkgray")
+    borders_sea.to_crs(proj).plot(ax=ax, color="darkgray")
 
     branch = grid_data.branch.copy()
     node = grid_data.node.copy()
@@ -107,18 +111,48 @@ def plot_map2(
 
     if width_col is not None:
         kwargs["linewidth"] = (gdf_edges[width_col[0]] / width_col[1]).clip(upper=width_col[2])
-    gdf_edges.plot(ax=ax, **kwargs)
+    if not gdf_edges.empty:
+        gdf_edges.to_crs(proj).plot(ax=ax, **kwargs)
     if node_options is None:
         node_options = {}
-    gdf_nodes.plot(ax=ax, **node_options)
+    if not gdf_nodes.empty:
+        gdf_nodes.to_crs(proj).plot(ax=ax, **node_options)
+    else:
+        print("DEBUG: No new nodes to plot")
 
     if include_generators:
-        gdf_generators = gdf_generators[gdf_generators["type"].isin(include_generators)]
+        # TODO this messes up gen_options markersize and alpha vectors
+        m_gen_keep = gdf_generators["type"].isin(include_generators)
+        gdf_generators = gdf_generators[m_gen_keep]
+        ind_gen_keep = gdf_generators.index
+        if ("alpha" in gen_options) and (type(gen_options["alpha"]) == pd.Series):
+            gen_options["alpha"] = gen_options["alpha"].loc[ind_gen_keep]
+        if ("markersize" in gen_options) and (type(gen_options["markersize"]) == pd.Series):
+            # print("gen markersize:",gen_options["markersize"].shape)
+            gen_options["markersize"] = gen_options["markersize"].loc[ind_gen_keep]
+
         if gen_options is None:
             gen_options = {}
-        gdf_generators.plot(ax=ax, **gen_options)
+        if not gdf_generators.empty:
+            gdf_generators.to_crs(proj).plot(ax=ax, **gen_options)
+            gdf_gen_edges = gdf_generators.copy()
+            gdf_gen_edges["geometry_x"] = geopandas.points_from_xy(gdf_gen_edges["lon"], gdf_gen_edges["lat"])
+            gdf_gen_edges["geometry_y"] = geopandas.points_from_xy(gdf_gen_edges["lon_node"], gdf_gen_edges["lat_node"])
+            gdf_gen_edges_geometry = gdf_gen_edges.apply(
+                lambda x: shapely.geometry.LineString([x["geometry_x"], x["geometry_y"]]), axis=1
+            )
+            gdf_gen_edges = geopandas.GeoDataFrame(gdf_gen_edges, geometry=gdf_gen_edges_geometry, crs="EPSG:4326")
+            # gdf_gen_edges.set_index("index")
+            gdf_gen_edges.to_crs(proj).plot(ax=ax, zorder=0, **gen_options)
 
-    ax.set_xlim(grid_data.node["lon"].min() - 1, grid_data.node["lon"].max() + 1)
-    ax.set_ylim(grid_data.node["lat"].min() - 1, grid_data.node["lat"].max() + 1)
+    if latlon is None:
+        latlon = {
+            "lat": (grid_data.node["lat"].min() - 1, grid_data.node["lat"].max() + 1),
+            "lon": (grid_data.node["lon"].min() - 1, grid_data.node["lon"].max() + 1),
+        }
+    p1 = pyproj.Proj(proj, preserve_units=False)
+    minmax = p1(latlon["lon"], latlon["lat"])
+    ax.set_xlim(minmax[0][0], minmax[0][1])
+    ax.set_ylim(minmax[1][0], minmax[1][1])
     ax.set(xlabel=None, ylabel=None)
     return
